daf0be335f5b14e01e55a4d78c9db7036b589159
pwmurphy89 <pwmurphy89@gmail.com>
2016-05-01 17:48:52 -0400

    added 'neededVotes' display

--- js/controller.js
+++ js/controller.js
@@ -1,66 +1,68 @@
 var interactiveMap = angular.module("interactiveMap",[]);
 interactiveMap.controller("interactiveMapCtrl",function($scope){
 	resetStates();
-	$scope.states=states;
 	calculateStateTotals();
+	$scope.states=states;
+
+	var neededRedVotes = 270 - $scope.redStateVotes;
+	$scope.neededRedVotes = neededRedVotes;
+	var neededBlueVotes = 270 - $scope.blueStateVotes;
+	$scope.neededBlueVotes = neededBlueVotes;
+
 	$scope.resetStates = function(){
 		resetStates();
 		calculateStateTotals();
 		$scope.states = states;
+		$scope.neededRedVotes = neededRedVotes;
+		$scope.neededBlueVotes = neededBlueVotes;
 	}
+
 	$scope.stateClicked = function(state){
 		var newColor = getNewColor(state);
 	}
 
-function getNewColor(state){
+	function getNewColor(state){
+		if(state.stateColor === "red"){
+			state.stateColor = "blue";
+			$scope.blueStateVotes += state.electoralVotes;
+			$scope.redStateVotes -= state.electoralVotes;
 
-	if(state.stateColor === "red"){
-		state.stateColor = "blue";
-		$scope.blueStateVotes += state.electoralVotes;
-		$scope.redStateVotes -= state.electoralVotes;
+		}else if(state.stateColor === "blue"){
+			state.stateColor = "open";
+			$scope.openStateVotes += state.electoralVotes;
+			$scope.blueStateVotes -= state.electoralVotes;
+			
+		}else if(state.stateColor === "open"){
+			state.stateColor = "red";
+			$scope.redStateVotes += state.electoralVotes;
+			$scope.openStateVotes -= state.electoralVotes;
+		}
 
-	}else if(state.stateColor === "blue"){
-		state.stateColor = "open";
-		$scope.openStateVotes += state.electoralVotes;
-		$scope.blueStateVotes -= state.electoralVotes;
-		
-	}else if(state.stateColor === "open"){
-		state.stateColor = "red";
-		$scope.redStateVotes += state.electoralVotes;
-		$scope.openStateVotes -= state.electoralVotes;
+		$scope.blueWidth = (($scope.blueStateVotes / 538) * 100) + '%';
+		$scope.redWidth = (($scope.redStateVotes / 538) * 100) + '%';
+		$scope.openWidth = (($scope.openStateVotes / 538) * 100) + '%';
+
+		$scope.neededRedVotes = 270 - $scope.redStateVotes;
+		$scope.neededBlueVotes = 270 - $scope.blueStateVotes;
 	}
 
-	$scope.blueWidth = (($scope.blueStateVotes / 538) * 100) + '%';
-	$scope.redWidth = (($scope.redStateVotes / 538) * 100) + '%';
-	$scope.openWidth = (($scope.openStateVotes / 538) * 100) + '%';
-}
+	function calculateStateTotals(){
+		$scope.redStateVotes = 0;
+		$scope.openStateVotes = 0;
+		$scope.blueStateVotes = 0;
 
-function calculateStateTotals(){
-	$scope.redStateVotes = 0;
-	$scope.openStateVotes = 0;
-	$scope.blueStateVotes = 0;
-
-	for(i=0; i < numStates; i++){
-		if(blueStates[i]){
-			$scope.blueStateVotes += blueStates[i].electoralVotes;
-		}else if(redStates[i]){
-			$scope.redStateVotes += redStates[i].electoralVotes;
-		}else if(openStates[i]){
-			$scope.openStateVotes += openStates[i].electoralVotes;
-		}
+		for(i=0; i < numStates; i++){
+			if(blueStates[i]){
+				$scope.blueStateVotes += blueStates[i].electoralVotes;
+			}else if(redStates[i]){
+				$scope.redStateVotes += redStates[i].electoralVotes;
+			}else if(openStates[i]){
+				$scope.openStateVotes += openStates[i].electoralVotes;
+			}
 		}
 		$scope.blueWidth = (($scope.blueStateVotes / 538) * 100) + '%';
 		$scope.redWidth = (($scope.redStateVotes / 538) * 100) + '%';
 		$scope.openWidth = (($scope.openStateVotes / 538) * 100) + '%';
-		console.log($scope.openStateVotes);
 	}
 
-})
-
-
-
-// for (var i=0;i<states[i].length;i++){
-// 	      if(states[i].isSmall){
-//             $scope.caption = states[i];
-//         }  
-// }
+});
